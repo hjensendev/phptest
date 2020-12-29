d51f6bdd69b2fb02ea0a04ea8fe8111e9b6ade22
Håkon Jensen <hakon@sicra.no>
2020-12-29 15:59:50 +0100

    Update index.php

--- index.php
+++ index.php
@@ -5,12 +5,6 @@ set_error_handler(function () {
     throw new Exception('Error when processing');
 });
 
-function DownloadAndProcessJSON()
-{
-    //Execute rest of bussiness logic here...
-    http_response_code(204);
-    exit;
-}
 
 // Validate code before trying to download and process json
 $valid_code = getenv('FUNCTION_CODE'); 
@@ -36,5 +30,11 @@ try {
     exit;
 }
 
+function DownloadAndProcessJSON()
+{
+    //Execute rest of bussiness logic here...
+    http_response_code(204);
+    exit;
+}
 
 ?>
